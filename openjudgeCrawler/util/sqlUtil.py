e3678804314d00dabf12c8fe8c3b77824b3cb4ba
abmcar <abmcar@qq.com>
2021-10-01 23:11:38 +0800

    [ADD] 修复项目路径

--- openjudgeCrawler/util/sqlUtil.py
+++
@@ -1,6 +0,0 @@
-import pymysql
-
-if __name__ == '__main__':
-    db = pymysql.connect(host="db.abmcar.top", port=10019, user="nyist", password="password=NULL0",
-                         database="NYIST_ACM")
-    cursor = db.cursor()
